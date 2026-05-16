0487aeb3cb9d017eefc1cf7222f4929ee7d88ea7
Ankush <ranawebdesginer@gmail.com>
2026-05-16 06:36:20 +0530

    sidebar sticky

--- components/pages/HomePage/components/WhyChooseUs/WhyChooseUs.tsx
+++ components/pages/HomePage/components/WhyChooseUs/WhyChooseUs.tsx
@@ -42,7 +42,7 @@ export default function WhyUs() {
   };
 
   return (
-    <section className={cx('why-sec')} id="why">
+    <section className={cx('why-sec')} id="why-choose">
       <div className={cx('why-inner')}>
         <div className={cx('why-header', 'reveal')}>
           <div className={cx('why-header-left')}>
