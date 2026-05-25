c28c33725e32568caef52406bf0b38feb4ced66f
Ankush <ranawebdesginer@gmail.com>
2026-05-26 02:59:44 +0530

    roboto file updatd

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,13 @@
 import type { MetadataRoute } from 'next';
 
+const siteUrl = 'https://shaanpainting.com.au';
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: '/sitemap.xml',
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
